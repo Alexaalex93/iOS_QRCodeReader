6859eeeb8e8f845f570eb9106fa234249e021227
cice <formador@aula1PovMacNFOR.local>
2017-02-22 11:03:01 +0100

    Initial Commit

---
+++ QRCodeReader/QRCodeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  QRCodeViewController.swift
+//  QRCodeReader
+//
+//  Created by Pablo Mateo Fernández on 27/01/2017.
+//  Copyright © 2017 355 Berry Street S.L. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
